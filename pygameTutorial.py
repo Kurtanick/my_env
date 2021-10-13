dd4a442b93237868e62b445f003f130cabade5dc
mkurt26 <mkurtanick@gmail.com>
2021-10-12 21:48:09 -0400

    Finished Tutorial

--- pygameTutorial.py
+++ pygameTutorial.py
@@ -20,6 +20,7 @@ from pygame.locals import (
     QUIT,
 )
 from pygame.sprite import Group
+from pygame.threads import init
 
 #Define constants for the screen width and height
 SCREEN_WIDTH = 800
@@ -78,8 +79,39 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+#Define cloud object
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("cloud.png").convert()
+        self.surf.set_colorkey((0,0,0), RLEACCEL)
+        #Starting position randomly generated
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+
+    def update(self):
+        #move based on constant speed
+        self.rect.move_ip(-5,0)
+        #remove when it passes the edge
+        if self.rect.right < 0:
+            self.kill()
+
+# Setup for sounds
+pygame.mixer.init()
+
 # Initialize pygame
 pygame.init()
+
+#Sound stuff
+pygame.mixer.music.load("background.mp3")
+pygame.mixer.music.play(loops=-1)
+
+#Sound effects
+collision_sound = pygame.mixer.Sound("glass.ogg")
 
 #Create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -87,6 +119,9 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 #Create a custom event for addingg a new enemy
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 250)
+#Custom event for adding clouds
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
 
 #Instantiate the player
 player = Player()
@@ -95,8 +130,12 @@ player = Player()
 # - enemies is used for collision detection and position updates
 # - all_sprites is used for rendering
 enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
+
+#Setup the clock for framerate
+clock = pygame.time.Clock()
 
 #Variable to keep the main loop running 
 running = True
@@ -120,6 +159,12 @@ while running:
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
+        
+        # Add a new cloud?
+        elif event.type == ADDCLOUD:
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
 
     #Get all keys currently pressed
     pressed_keys = pygame.key.get_pressed()
@@ -127,11 +172,12 @@ while running:
     #Update the player sprite based on kepresses
     player.update(pressed_keys)
 
-    #Update enemy position
+    #Update enemy and cloud positions
     enemies.update()
+    clouds.update()
 
-    #Fill the screnn with black
-    screen.fill((0,0,0))
+    #Fill the screnn with black (blue)
+    screen.fill((17,25,33))
 
     # #Draw the player on the screen
     # screen.blit(player.surf, player.rect)
@@ -144,10 +190,18 @@ while running:
     if pygame.sprite.spritecollideany(player,enemies):
         #remove the player and stop the loop
         player.kill()
+        collision_sound.play()
         running = False
 
     #Flip everything to the display
     pygame.display.flip()
+
+    # Ensure 30 fps
+    clock.tick(60)
+
+# All done! Stop and quit the mixer.
+pygame.mixer.music.stop()
+pygame.mixer.quit()
 
 
 
